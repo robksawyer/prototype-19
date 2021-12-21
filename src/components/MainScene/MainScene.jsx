42a8262d7c0e981e3f22240755a0cfed85c218bf
Rob Sawyer <robksawyer@gmail.com>
2021-12-22 05:41:34 +0900

    Got some clouds in there

--- src/components/MainScene/MainScene.jsx
+++ src/components/MainScene/MainScene.jsx
@@ -6,17 +6,6 @@ import PropTypes from 'prop-types';
 import useErrorBoundary from 'use-error-boundary';
 
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
-
-import FoilBalloonZero from './FoilBalloonZero';
-import FoilBalloonTwo from './FoilBalloonTwo';
-
-// Enabled for effects
-// import {
-//   EffectComposer,
-//   // Bloom,
-//   // ChromaticAberration,
-// } from '@react-three/postprocessing'
-
 import * as THREE from 'three';
 import {
   useHelper,
@@ -24,14 +13,23 @@ import {
   useTexture,
   OrbitControls,
   Stats,
+  Cloud,
   Environment,
 } from '@react-three/drei';
 // import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import * as STDLIB from 'three-stdlib';
+// Enabled for effects
+// import {
+//   EffectComposer,
+//   // Bloom,
+//   // ChromaticAberration,
+// } from '@react-three/postprocessing'
 
 import styles from './MainScene.module.css';
 
 import Loader from '@/components/Loader';
+import FoilBalloonZero from './FoilBalloonZero';
+import FoilBalloonTwo from './FoilBalloonTwo';
 
 // Shader stack
 import './shaders/defaultShaderMaterial';
@@ -76,7 +74,10 @@ const Scene = () => {
   const group = React.useRef();
 
   const spotLight = React.useRef();
-  const pointLight = React.useRef();
+  const pointLight0 = React.useRef();
+  const pointLight1 = React.useRef();
+  const pointLight2 = React.useRef();
+  const pointLight3 = React.useRef();
 
   // Texture loading example
   const texture = useTexture('/3d/textures/checkerboard.jpg');
@@ -106,41 +107,49 @@ const Scene = () => {
   //   [scene],
   // );
 
+  useHelper(spotLight, THREE.SpotLightHelper, 'teal');
+  useHelper(pointLight0, THREE.PointLightHelper, 0.5, 'hotpink');
+  useHelper(pointLight1, THREE.PointLightHelper, 0.5, 'hotpink');
+  useHelper(pointLight2, THREE.PointLightHelper, 0.5, 'hotpink');
+  useHelper(pointLight3, THREE.PointLightHelper, 0.5, 'hotpink');
+  useHelper(mesh, THREE.BoxHelper, '#272740');
+  useHelper(mesh, STDLIB.VertexNormalsHelper, 1, '#272740');
+
   return (
     <>
-      <pointLight position={[-10, 0, -20]} color="lightblue" intensity={2.5} />
+      <pointLight position={[-10, 0, -20]} color="lightblue" intensity={0.5} />
       <group ref={group}>
         <pointLight
-          ref={pointLight}
+          ref={pointLight0}
           color="#0051FF"
-          position={[4, 4, 0]}
-          intensity={5}
+          position={[-4, 0, -2]}
+          intensity={1}
         />
         <pointLight
-          ref={pointLight}
+          ref={pointLight1}
           color="#08FF88"
-          position={[4, 4, 0]}
-          intensity={5}
+          position={[4, 1, 2]}
+          intensity={1}
         />
         <pointLight
-          ref={pointLight}
+          ref={pointLight2}
           color="#FFF035"
-          position={[4, 4, 0]}
-          intensity={5}
+          position={[-4, 0, 3]}
+          intensity={1}
         />
         <pointLight
-          ref={pointLight}
+          ref={pointLight3}
           color="#DF1CFF"
-          position={[4, 4, 0]}
-          intensity={5}
+          position={[10, 0, -1]}
+          intensity={1}
         />
       </group>
       <spotLight
         castShadow
-        position={[2, 5, 2]}
+        position={[0, 0, 10]}
         ref={spotLight}
         angle={0.5}
-        distance={20}
+        distance={5}
       />
       <FoilBalloonTwo position={[-4, -3, 0]} />
       <FoilBalloonZero position={[0, -3, 0]} />
@@ -177,16 +186,16 @@ const MainScene = ({
         {/* https://github.com/pmndrs/react-three-fiber/blob/master/markdown/api.md#canvas */}
         <Canvas
           pixelRatio={window.devicePixelRatio || 1}
-          // colorManagement
-          // shadowMap
+          colorManagement
+          shadowMap
           camera={{ position: [-5, 3, 5] }}
-          // onCreated={({ gl }) => {
-          //   gl.physicallyCorrectLights = true;
-          //   // gl.toneMapping = THREE.ACESFilmicToneMapping
-          //   gl.outputEncoding = THREE.sRGBEncoding;
-          // }}
+          onCreated={({ gl }) => {
+            gl.physicallyCorrectLights = true;
+            // gl.toneMapping = THREE.ACESFilmicToneMapping
+            gl.outputEncoding = THREE.sRGBEncoding;
+          }}
           style={{
-            background: '#000000',
+            background: '#FE0081',
           }}
         >
           <fog attach="fog" args={['floralwhite', 0, 20]} />
@@ -197,11 +206,19 @@ const MainScene = ({
               </Html>
             }
           >
+            <Cloud position={[-4, -2, 0]} args={[3, 2]} />
+            <Cloud position={[-4, 2, 0]} args={[3, 2]} />
+            <Cloud args={[3, 2]} />
+            <Cloud position={[4, -2, 0]} args={[3, 2]} />
+            <Cloud position={[4, 2, 0]} args={[3, 2]} />
+
             <Scene />
+
             <Environment preset="studio" />
           </React.Suspense>
 
           {/* <Effects /> */}
+          {/* <fog args={['black', 100, 700]} /> */}
           <OrbitControls />
         </Canvas>
       </ErrorBoundary>
